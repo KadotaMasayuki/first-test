95e40f739ac8341633f92c22dbd6ad3b9c53343e
KadotaMasayuki <44630941+KadotaMasayuki@users.noreply.github.com>
2019-12-07 21:23:04 +0900

    Add files via upload

---
+++ a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('topic', help='pleas set topic', type=str)
+
+args = parser.parse_args()
+
+if (args.topic) :
+  print('topic={0}'.format(args.topic))
+
